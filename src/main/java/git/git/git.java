0e5f3f985514988e7b181a095ab800b5a1183645
Diego Hilazo Parra <diego@example.com>
2020-01-29 15:17:02 +0100

    añadido

--- src/main/java/git/git/git.java
+++ src/main/java/git/git/git.java
@@ -15,7 +15,9 @@ public class git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        
+        System.out.println("hola mundo");
     }
     
 }
